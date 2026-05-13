0b5c5f14b529fa1d3f82b8063f5335c786a91e04
Grzegorz Dynak <77176069+gregusio@users.noreply.github.com>
2026-05-13 11:14:35 +0200

    Feature/ef6 integration (#3)
    * Add entity models and DTOs for reporting features
    
    * Add Entity Framework and AdventureWorks data models
    
    * Add AdventureWorksContext for EF data access
    
    * Add ReportsController with reporting and CSV export APIs

---
+++ src/AdventureWorksReports.Legacy/Models/DTOs/MonthlyCategoryProfitDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AdventureWorksReports.Legacy.Models.DTOs
+{
+    public class MonthlyCategoryProfitDto
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public string CategoryName { get; set; }
+        public decimal TotalProfit { get; set; }
+    }
+}
