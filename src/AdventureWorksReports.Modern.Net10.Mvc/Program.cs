882604e3e676c0b12be5f71fd2cc3a2ea7eec29a
gregusio <grzegorzdynaq@gmail.com>
2026-06-01 13:53:35 +0200

    Updated NuGet packages in the project to latest versions

--- src/AdventureWorksReports.Modern.Net10.Mvc/Program.cs
+++ src/AdventureWorksReports.Modern.Net10.Mvc/Program.cs
@@ -1,5 +1,7 @@
 using AdventureWorksReports.Modern.Net10.Mvc.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +15,14 @@ builder.Services.AddDbContext<AdventureWorksContext>(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "AdventureWorksReports API",
+        Version = "v1",
+    });
+});
 
 var app = builder.Build();
 
@@ -21,7 +30,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "AdventureWorksReports API v1");
+        c.RoutePrefix = String.Empty;
+    });
 }
 
 app.UseAuthorization();
