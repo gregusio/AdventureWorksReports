167ce0796cde073c71d6dc90fcd6dc847930d938
gregusio <grzegorzdynaq@gmail.com>
2026-07-07 00:20:32 +0200

    Update restore-database.sql to include GO statements and ensure proper database restoration and truncation

--- restore-database.sql
+++ restore-database.sql
@@ -1,4 +1,11 @@
 RESTORE DATABASE AdventureWorks
 FROM DISK = '/var/opt/mssql/backup/AdventureWorks.bak'
 WITH MOVE 'AdventureWorks2022' TO '/var/opt/mssql/data/AdventureWorks.mdf',
-MOVE 'AdventureWorks2022_log' TO '/var/opt/mssql/data/AdventureWorks_log.ldf';
+     MOVE 'AdventureWorks2022_log' TO '/var/opt/mssql/data/AdventureWorks_log.ldf';
+GO
+
+USE AdventureWorks;
+GO
+
+TRUNCATE TABLE Production.ProductReview;
+GO
